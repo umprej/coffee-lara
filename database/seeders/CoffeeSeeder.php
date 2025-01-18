ede9757994079caf4b2b55eac4fcace41df86b59
umprej <umprejan@office365.zcu.cz>
2025-01-19 00:11:27 +0100

    Coffee model, seeder, images

---
+++ database/seeders/CoffeeSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CoffeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $coffees = [
+            [
+                'title' => 'Espresso',
+                'description' => 'A concentrated form of coffee served in small, strong shots. Made by forcing hot water under pressure through very finely ground coffee beans.',
+                'image_path' => 'coffee_images/espresso.jpg',
+            ],
+            [
+                'title' => 'Cappuccino',
+                'description' => 'An espresso-based coffee drink that is traditionally prepared with steamed milk foam. Equal parts espresso, steamed milk, and milk foam.',
+                'image_path' => 'coffee_images/cappuccino.jpg',
+            ],
+            [
+                'title' => 'Latte',
+                'description' => 'A latte is a creamy coffee drink made with a shot of espresso and steamed milk, topped with a thin layer of foam. Its smooth, velvety texture and balanced flavor make it a popular choice for those who enjoy a milder coffee experience.',
+                'image_path' => 'coffee_images/latte.jpg',
+            ]
+        ];
+
+        foreach ($coffees as $coffee) {
+            Coffee::create($coffee);
+        }
+    }
+}
