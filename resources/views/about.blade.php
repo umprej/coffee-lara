b7d0dddc5ece327b2cec535510c5f6a44df8c96e
3rr0rkaCZ <hana.beranova1@gmail.com>
2025-02-26 00:03:50 +0100

    Read me update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,9 +3,23 @@
 @section('title', 'About Us')
 
 @section('content')
-    <div class="container mx-auto px-4 py-8">
-        <h1 class="text-4xl font-bold text-center mb-8">About Us</h1>
-        <p class="text-center text-lg">Welcome to our coffee shop! We are passionate about providing the best coffee experience.</p>
-		<p class="text-center text-lg">Welcome to our coffee shop! We are passionate about providing the best coffee experience.</p>
+<section id="pozadi">
+    <div  class="container mx-auto px-4 py-8">
+        <h1 class="nadpis" class="text-4xl font-bold text-center mb-8">About Us</h1>
+        <div class="menu-onas">
+    
+        
+            <div class="letec">
+            <img class="onas" src="storage/coffee_images/onas.png" alt="">
+            </div>
+            <div class="letec2">
+            <p class="text-onas" class="text-center text-lg">Welcome to our coffee shop! We are passionate about providing the best coffee experience.</p> <br>
+		    <p class="text-onas" class="text-center text-lg">We want to help you with your choice of a coffee and how to prepare it. You can also add new drinks of your choice and the instroctions for other to enjoy it too!
+              </p> <br><p class="text-onas" class="text-center text-lg">  This was done as a school project from HaLean Trio hoping to do something useful and also to not drop out.</p>
+            
+            </div>
+        </div>
     </div>
+
+</section>
 @endsection
