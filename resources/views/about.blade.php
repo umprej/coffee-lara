ec47d3d2cebcd7c3c86354dd4456e2176f6c3f80
3rr0rkaCZ <hana.beranova1@gmail.com>
2025-02-26 00:50:17 +0100

    Section Show

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,12 +10,12 @@
     
         
             <div class="letec">
-            <img class="onas" src="storage/coffee_images/onas.png" alt="">
+            <img class="obrazek-leva" src="storage/coffee_images/onas.png" alt="">
             </div>
             <div class="letec2">
-            <p class="text-onas" class="text-center text-lg">Welcome to our coffee shop! We are passionate about providing the best coffee experience.</p> <br>
-		    <p class="text-onas" class="text-center text-lg">We want to help you with your choice of a coffee and how to prepare it. You can also add new drinks of your choice and the instroctions for other to enjoy it too!
-              </p> <br><p class="text-onas" class="text-center text-lg">  This was done as a school project from HaLean Trio hoping to do something useful and also to not drop out.</p>
+            <p class="text-prava" class="text-center text-lg">Welcome to our coffee shop! We are passionate about providing the best coffee experience.</p> <br>
+		    <p class="text-prava" class="text-center text-lg">We want to help you with your choice of a coffee and how to prepare it. You can also add new drinks of your choice and the instroctions for other to enjoy it too!
+              </p> <br><p class="text-prava" class="text-center text-lg">  This was done as a school project from HaLean Trio hoping to do something useful and also to not drop out.</p>
             
             </div>
         </div>
