124b37f42f8f0937b30c13f7033dc6f9d9bf91b0
Leo H <le0.hanus03@gmail.com>
2025-02-25 15:19:59 +0100

    Star is now changing colors when clicked

--- resources/views/coffee/show.blade.php
+++ resources/views/coffee/show.blade.php
@@ -3,10 +3,10 @@
 @section('title', $coffee->title)
 
 @section('content')
-    <div class="relative">
+<div class="relative">
         <div class="absolute top-0 right-0 m-4">
-            <a href="#" class="text-yellow-500">
-                <i class="fas fa-star text-2xl"></i>
+            <a href="#" class="text-yellow-500" id="favorite-star">
+                <i class="far fa-star text-2xl"></i>
             </a>
         </div>
     </div>
@@ -21,4 +21,15 @@
             <p class="text-lg">{{ $coffee->description }}</p>
         </div>
     </div>
+<script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const star = document.getElementById('favorite-star');
+            star.addEventListener('click', function(event) {
+                event.preventDefault();
+                const icon = star.querySelector('i');
+                icon.classList.toggle('far'); // Toggle the 'far' class (Font Awesome Regular) to show an empty star
+        		icon.classList.toggle('fas'); // Toggle the 'fas' class (Font Awesome Solid) to show a filled star
+            });
+        });
+    </script>
 @endsection
