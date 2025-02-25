bb781770f5193d3ddd7269dec2949859b3c0d96d
Leo H <le0.hanus03@gmail.com>
2025-02-25 21:15:55 +0100

    completed design of welcome page

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -11,9 +11,9 @@
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body class="bg-gray-100">
-    <header class="bg-gray-800 text-white py-4">
+    <header class="bg-gray-800 text-white py-4" id="custom-header">
         <div class="container mx-auto flex justify-between items-center px-4">
-            <a href="{{ route('root') }}" class="text-2xl font-bold">Coffee Types</a>
+            <a href="{{ route('root') }}" class="text-2xl font-bold">HaLean Coffee</a>
             <nav>
                 <a href="{{ route('about') }}" class="px-4 py-2 hover:bg-gray-700 rounded transition">About Us</a>
                 @auth
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,23 +4,23 @@
 
 @section('content')
     <section id="menukafe1">
-        <main id="pozadi">
-            <main class="hlavni">
-                <br>
-                <div grid grid-cols-3 class="menu-grid"> <br>
-                    <h1 class="nadpis">Coffee & Spirits</h1> <br>
+        <div id="pozadi">
+            <div class="hlavni">
+                <h1 class="nadpis">Coffee drinks</h1>
 
+                <div class="menu-grid grid grid-cols-3 gap-4">
                     @foreach($coffees as $coffee)
                         @if($coffee->image_path)
-                            <a href="{{ route('coffee.show', $coffee->id) }}" class=""><img class="produkt"
-                            src="{{ asset('storage/' . $coffee->image_path) }}"
-                            alt="{{ $coffee->title }}"> 
-                            </a>
-                            <br> <br>
+                            <div class="text-center">
+                                <a href="{{ route('coffee.show', $coffee->id) }}">
+                                    <img class="produkt" src="{{ asset('storage/' . $coffee->image_path) }}" alt="{{ $coffee->title }}">
+                                </a>
+                                <p class="text-white mt-2">{{ $coffee->title }}</p>
+                            </div>
                         @endif
                     @endforeach
                 </div>
-            </main>
-        </main>
+            </div>
+        </div>
     </section>
 @endsection
