47e3eeb81e58c23ae678fda606698dfd5b6194d7
3rr0rkaCZ <hana.beranova1@gmail.com>
2025-02-26 13:15:09 +0100

    Create Visual

--- resources/views/coffee/create.blade.php
+++ resources/views/coffee/create.blade.php
@@ -3,23 +3,27 @@
 @section('title', 'Add New Coffee Type')
 
 @section('content')
-    <h1 class="">Add New Coffee</h1>
+<div id="pozadi">
+    <h1 class="nadpis">Add New Coffee</h1>
     <div class="hlavni">
+
+    <div class="menu-create">
         <form action="" method="POST" enctype="multipart/form-data">
             @csrf
-            <div class="">
-                <label for="title" class="">Title</label>
+            <div class="create-center">
+                <label for="title" class="napis">Title</label> <br>
                 <input type="text" name="title" id="title" class="w-full border-gray-300 rounded" required>
             </div>
-            <div class="">
-                <label for="description" class="">Description</label>
+            <div class="create-center">
+                <label for="description" class="napis">Description</label> <br>
                 <textarea name="description" id="description" class="w-full border-gray-300 rounded" required></textarea>
             </div>
-            <div class="">
-                <label for="image_path" class="">Image</label>
+            <div class="create-center">
+                <label for="image_path" class="napis">Image</label> <br>
                 <input type="file" name="image_path" id="image_path" class="w-full border-gray-300 rounded">
-            </div>
-            <button type="submit" class="">Add Coffee</button>
+            </div> <br>
+            <div class="create-center">
+            <button type="submit" class="submit">Add Coffee</button></div>
         </form>
-    </div>
+    </div></div></div>
 @endsection
